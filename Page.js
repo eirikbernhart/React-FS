0361c1665ccca0c3565d1de519516a59fa8b2f38
bernieboy <bereir15@student.westerdals.no>
2017-10-26 19:16:05 +0200

    tiny progress..

---
+++ Page.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class Page extends Component {
+    render() {
+        return <Text>Text from external component</Text>
+    }
+}
